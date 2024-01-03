c7d9dd57eb9f403ae718a0f51c6548dbbb860963
Bimantara <bimantarasc@gmail.com>
2024-01-03 08:48:30 +0700

    vscode cloud test

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <div>
-        hello this is staging
+        Hello this is staging
       </div>
     </>
   )
