69ccb855b395fa5614d7ea090664eeb993fbc5bd
majid <bimantarasc@gmail.com>
2024-01-06 11:52:59 +0700

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import App from './app';
 import './index.css';
-import App from './app.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
